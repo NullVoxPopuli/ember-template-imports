ea33508f9f423b1e4f55234e67245adc5f58f561
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-08-12 13:09:21 -0400

    Update ember-try.js

--- config/ember-try.js
+++ config/ember-try.js
@@ -93,6 +93,7 @@ module.exports = async function () {
         npm: {
           devDependencies: {
             'ember-source': release,
+            'ember-cli': '^5.0.0',
             ...ember5Deps,
           },
         },
